a9bb7bd639acc1aef7edcbf63ea3757609755be4
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-12-03 01:29:35 -0500

    update

---
+++ data.js
@@ -0,0 +1,88 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-the-annunciation",
+      "name": "1. The Annunciation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.03217949870061787,
+        "pitch": 0,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6327312801355109,
+          "pitch": -0.002123553522650923,
+          "rotation": 0,
+          "target": "1-2-the-visitation"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-the-visitation",
+      "name": "2. The Visitation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
