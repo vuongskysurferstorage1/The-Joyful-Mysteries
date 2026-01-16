60fb0817f67553e310cef0123252f555f1d95e9f
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-01-16 01:23:22 -0500

    Update data.js

--- data.js
+++ data.js
@@ -42,8 +42,8 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": 1.1467385567477582,
-          "pitch": -0.2837348426881796,
+          "yaw": 1.1622634055138068,
+          "pitch": -0.28166625856094996,
           "title": "The Birth of Jesus Foretold",
           "text": "In the sixth month of Elizabeth’s pregnancy, God sent the angel Gabriel to Nazareth, a town in Galilee, to a virgin pledged to be married to a man named Joseph, a descendant of David. The virgin’s name was Mary. The angel went to her and said, “Greetings, you who are highly favored! The Lord is with you.”<div><br></div><div>Mary was greatly troubled at his words and wondered what kind of greeting this might be. But the angel said to her, “Do not be afraid, Mary; you have found favor with God. You will conceive and give birth to a son, and you are to call him Jesus. He will be great and will be called the Son of the Most High. The Lord God will give him the throne of his father David, and he will reign Jacob’s descendants forever; his kingdom will never end.”</div><div><br></div><div>“How will this be,” Mary asked the angel, “since I am a virgin?”</div><div><br></div><div>The angel answered, “The Holy Spirit will come on you, and the power of the Most High will overshadow you. So the holy one to be born will be called the Son of God. Even Elizabeth your relative is going to have a child in her old age, and she who was said to be unable to conceive is in her sixth month. For no word from God will ever fail.”</div><div><br></div><div>“I am the Lord’s servant,” Mary answered. “May your word to me be fulfilled.” Then the angel left her.</div><div>(Luke 1:26-38)</div>"
         }
@@ -97,8 +97,8 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": -0.1274290564116054,
-          "pitch": -0.6639379517695332,
+          "yaw": -0.12524373470416705,
+          "pitch": -0.6640726414363716,
           "title": "Mary Visits Elizabeth",
           "text": "At that time Mary got ready and hurried to a town in the hill country of Judea, where she entered Zechariah’s home and greeted Elizabeth. When Elizabeth heard Mary’s greeting, the baby leaped in her womb, and Elizabeth was filled with the Holy Spirit. In a loud voice she exclaimed: “Blessed are you among women, and blessed is the child you will bear! But why am I so favored, that the mother of my Lord should come to me? As soon as the sound of your greeting reached my ears, the baby in my womb leaped for joy. Blessed is she who has believed that the Lord would fulfill his promises to her!”&nbsp;<div>(Luke 1:39-45)</div>"
         }
@@ -152,8 +152,8 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": -0.1711255720480871,
-          "pitch": -0.41842781857553035,
+          "yaw": -0.16032498761056857,
+          "pitch": -0.4172683295906481,
           "title": "The Birth of Jesus",
           "text": "In those days Caesar Augustus issued a decree that a census should be taken of the entire Roman world. (This was the first census that took place while Quirinius was governor of Syria.) And everyone went to their own town to register.<div><br></div><div>So Joseph also went up from the town of Nazareth in Galilee to Judea, to Bethlehem the town of David, because he belonged to the house and line of David. He went there to register with Mary, who was pledged to be married to him and was expecting a child. While they were there, the time came for the baby to be born, and she gave birth to her firstborn, a son. She wrapped him in cloths and placed him in a manger, because there was no guest room available for them.</div><div><br></div><div>And there were shepherds living out in the fields nearby, keeping watch over their flocks at night. An angel of the Lord appeared to them, and the glory of the Lord shone around them, and they were terrified. But the angel said to them, “Do not be afraid. I bring you good news that will cause great joy for all the people. Today in the town of David a Savior has been born to you; he is the Messiah, the Lord. This will be a sign to you: You will find a baby wrapped in cloths and lying in a manger.”</div><div><br></div><div>Suddenly a great company of the heavenly host appeared with the angel, praising God and saying,</div><div><br></div><div>“Glory to God in the highest heaven, and on earth peace to those on whom his favor rests.”</div><div><br></div><div>When the angels had left them and gone into heaven, the shepherds said to one another, “Let’s go to Bethlehem and see this thing that has happened, which the Lord has told us about.”</div><div><br></div><div>So they hurried off and found Mary and Joseph, and the baby, who was lying in the manger. When they had seen him, they spread the word concerning what had been told them about this child, and all who heard it were amazed at what the shepherds said to them. But Mary treasured up all these things and pondered them in her heart. The shepherds returned, glorifying and praising God for all the things they had heard and seen, which were just as they had been told.</div><div>(Luke 2:1-20)</div>"
         }
@@ -193,8 +193,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.04657628693453475,
-          "pitch": -0.3609398172291023,
+          "yaw": 0.04448042282405673,
+          "pitch": -0.3609151715076102,
           "rotation": 0,
           "target": "4-5-the-finding-of-jesus-in-the-temple"
         },
@@ -207,8 +207,8 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": 0.006611037610330328,
-          "pitch": -0.5051260189314988,
+          "yaw": 0.004950021509621649,
+          "pitch": -0.5050909207814644,
           "title": "Jesus Presented in the Temple",
           "text": "When the time came for the purification rites required by the Law of Moses, Joseph and Mary took him to Jerusalem to present him to the Lord (as it is written in the Law of the Lord, “Every firstborn male is to be consecrated to the Lord”), and to offer a sacrifice in keeping with what is said in the Law of the Lord: “a pair of doves or two young pigeons.”<div><br></div><div>Now there was a man in Jerusalem called Simeon, who was righteous and devout. He was waiting for the consolation of Israel, and the Holy Spirit was on him. It had been revealed to him by the Holy Spirit that he would not die before he had seen the Lord’s Messiah. Moved by the Spirit, he went into the temple courts. When the parents brought in the child Jesus to do for him what the custom of the Law required, Simeon took him in his arms and praised God, saying:</div><div><br></div><div>“Sovereign Lord, as you have promised, you may now dismiss your servant in peace. For my eyes have seen your salvation, which you have prepared in the sight of all nations: a light for revelation to the Gentiles, and the glory of your people Israel.”</div><div><br></div><div>The child’s father and mother marveled at what was said about him. Then Simeon blessed them and said to Mary, his mother: “This child is destined to cause the falling and rising of many in Israel, and to be a sign that will be spoken against, so that the thoughts of many hearts will be revealed. And a sword will pierce your own soul too.”</div><div><br></div><div>There was also a prophet, Anna, the daughter of Penuel, of the tribe of Asher. She was very old; she had lived with her husband seven years after her marriage, and then was a widow until she was eighty-four. She never left the temple but worshiped night and day, fasting and praying. Coming up to them at that very moment, she gave thanks to God and spoke about the child to all who were looking forward to the redemption of Jerusalem.</div><div>(Luke 2:22-38)</div>"
         }
